519025867de11ba69a788873452c6060673603d8
amukhachov <wr1teFun>
2015-11-05 13:43:36 +0200

    messaging refactoring

--- src/org/whispersystems/whisperpush/service/WhisperPushMessageSender.java
+++ src/org/whispersystems/whisperpush/service/WhisperPushMessageSender.java
@@ -19,29 +19,37 @@ package org.whispersystems.whisperpush.service;
 import android.app.Activity;
 import android.app.PendingIntent;
 import android.content.Context;
-import android.text.TextUtils;
 import android.util.Log;
 
+import com.google.android.mms.ContentType;
+import com.google.android.mms.MmsException;
+import com.google.android.mms.pdu.CharacterSets;
+import com.google.android.mms.pdu.EncodedStringValue;
+import com.google.android.mms.pdu.PduBody;
+import com.google.android.mms.pdu.SendReq;
+
 import org.whispersystems.libaxolotl.util.guava.Optional;
-import org.whispersystems.textsecure.api.TextSecureAccountManager;
 import org.whispersystems.textsecure.api.TextSecureMessageSender;
 import org.whispersystems.textsecure.api.crypto.UntrustedIdentityException;
+import org.whispersystems.textsecure.api.messages.TextSecureAttachment;
+import org.whispersystems.textsecure.api.messages.TextSecureGroup;
 import org.whispersystems.textsecure.api.messages.TextSecureMessage;
-import org.whispersystems.textsecure.api.push.ContactTokenDetails;
 import org.whispersystems.textsecure.api.push.TextSecureAddress;
+import org.whispersystems.textsecure.api.push.exceptions.EncapsulatedExceptions;
 import org.whispersystems.textsecure.api.push.exceptions.UnregisteredUserException;
 import org.whispersystems.textsecure.api.util.InvalidNumberException;
-import org.whispersystems.textsecure.api.util.PhoneNumberFormatter;
 import org.whispersystems.whisperpush.WhisperPush;
 import org.whispersystems.whisperpush.api.OutgoingMessage;
-import org.whispersystems.whisperpush.directory.Directory;
-import org.whispersystems.whisperpush.directory.NotInDirectoryException;
 import org.whispersystems.whisperpush.util.StatsUtils;
 import org.whispersystems.whisperpush.util.Util;
 import org.whispersystems.whisperpush.util.WhisperPreferences;
 import org.whispersystems.whisperpush.util.WhisperServiceFactory;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 public class WhisperPushMessageSender {
@@ -69,7 +77,7 @@ public class WhisperPushMessageSender {
         this.whisperPush = WhisperPush.getInstance(context);
     }
 
-    public boolean sendMessage(OutgoingMessage message) {
+    public boolean sendTextMessage(OutgoingMessage message) {
         Log.d(TAG, "Got outgoing message");
 
         List<String> destinations = message.getDestinations();
@@ -107,6 +115,144 @@ public class WhisperPushMessageSender {
             abortSendOperation(message, e);
         }
         return false;
+    }
+
+    public void sendMultimediaMessage(SendReq message, List<TextSecureAttachment> attachments)
+            throws MmsException, UntrustedIdentityException {
+        send(message, attachments, null);
+    }
+
+    public void sendGroupMessage(SendReq message, List<TextSecureAttachment> attachments, byte[] id)
+            throws MmsException, UntrustedIdentityException {
+        send(message, attachments, new TextSecureGroup(id));
+    }
+
+    private void send(SendReq message, List<TextSecureAttachment> attachments, TextSecureGroup textSecureGroup)
+            throws MmsException, UntrustedIdentityException {
+        TextSecureMessageSender messageSender = WhisperServiceFactory.createMessageSender(context);
+        List<TextSecureAddress> recipients = getGroupRecipients(message);
+
+        try {
+            String body = getMessageText(message.getBody());
+            TextSecureMessage.Builder builder = TextSecureMessage.newBuilder()
+                    .withBody(body)
+                    .withAttachments(attachments);
+            if (textSecureGroup != null) {
+                builder.asGroupMessage(textSecureGroup);
+            }
+
+            messageSender.sendMessage(recipients, builder.build());
+        } catch (IOException e) {
+            Log.w(TAG, e);
+            throw new MmsException(e);
+        } catch (EncapsulatedExceptions eex) {
+            Log.w(TAG, eex);
+            throw new MmsException(eex);
+        }
+
+    }
+
+    public void sendGroupUpdate(byte[] id, Collection<String> members)
+            throws MmsException, UntrustedIdentityException {
+        TextSecureMessageSender messageSender = WhisperServiceFactory.createMessageSender(context);
+        List<TextSecureAddress> recipients = new ArrayList<>(members.size());
+
+        for (String destination : members) {
+            String e164number;
+            try {
+                e164number = whisperPush.formatNumber(destination);
+            } catch (InvalidNumberException e) {
+                Log.w(TAG, e);
+                throw new MmsException(e);
+            }
+            recipients.add(new TextSecureAddress(e164number));
+        }
+        members.add(whisperPush.getLocalNumber());
+
+        try {
+            TextSecureGroup textSecureGroup = new TextSecureGroup(
+                    TextSecureGroup.Type.UPDATE,
+                    id, null,
+                    new ArrayList<>(members),
+                    null);
+
+            messageSender.sendMessage(recipients,
+                    TextSecureMessage.newBuilder().asGroupMessage(textSecureGroup).build());
+
+        } catch (IOException e) {
+            Log.w(TAG, e);
+            throw new MmsException(e);
+        } catch (EncapsulatedExceptions eex) {
+            Log.w(TAG, eex);
+            throw new MmsException(eex);
+        }
+    }
+
+    private List<TextSecureAddress> getGroupRecipients(SendReq message) throws MmsException {
+        EncodedStringValue[] destinations = message.getTo();
+        EncodedStringValue[] values = message.getBcc();
+        List<EncodedStringValue> destinationsToSkip = values != null ? Arrays.asList(values) : null;
+        List<TextSecureAddress> recipients = new ArrayList<>(destinations.length);
+
+        for (EncodedStringValue destination : destinations) {
+            if (destinationsToSkip != null) {
+                boolean shouldContinue = false;
+                for (EncodedStringValue value : destinationsToSkip) {
+                    if (value.getString().equals(destination.getString())) {
+                        shouldContinue = true;
+                        break;
+                    }
+                }
+                if (shouldContinue) {
+                    continue;
+                }
+            }
+
+            String e164number;
+            try {
+                e164number = whisperPush.formatNumber(destination.getString());
+            } catch (InvalidNumberException e) {
+                Log.w(TAG, e);
+                throw new MmsException(e);
+            }
+            boolean recipientSupportsSecureMessaging = whisperPush.isRecipientSupportsSecureMessaging(e164number, true);
+            if (!recipientSupportsSecureMessaging) {
+                throw new MmsException("Recipient " + e164number + " doesn't support secure messaging");
+            }
+            recipients.add(new TextSecureAddress(e164number));
+        }
+
+        return recipients;
+    }
+
+    private String getMessageText(PduBody body) {
+        String bodyText = null;
+
+        for (int i = 0; i < body.getPartsNum(); i++) {
+            if (ContentType.isTextType(Util.toIsoString(body.getPart(i).getContentType()))) {
+                String partText;
+
+                try {
+                    String characterSet = CharacterSets.getMimeName(body.getPart(i).getCharset());
+
+                    if (characterSet.equals(CharacterSets.MIMENAME_ANY_CHARSET))
+                        characterSet = CharacterSets.MIMENAME_ISO_8859_1;
+
+                    if (body.getPart(i).getData() != null) {
+                        partText = new String(body.getPart(i).getData(), characterSet);
+                    } else {
+                        partText = "";
+                    }
+                } catch (UnsupportedEncodingException e) {
+                    Log.w("PartParser", e);
+                    partText = "Unsupported Encoding!";
+                }
+
+                bodyText = (bodyText == null) ? partText : bodyText + " " + partText;
+            }
+        }
+
+        return bodyText;
     }
 
     private void completeSendOperation(OutgoingMessage message) {
